efb02dadec7ddea33d9943c11df4231078e4b69d
narugo1992 <narugo992@gmail.com>
2024-03-17 20:54:09 +0800

    dev(narugo): add cxip

--- cwip_feats.py
+++ cwip_feats.py
@@ -6,7 +6,6 @@ import numpy as np
 import pandas as pd
 import torch
 from ditk import logging
-from hbutils.reflection import progressive_for
 from huggingface_hub import hf_hub_download
 from natsort import natsorted
 from rainbowneko.infer import WorkflowRunner, LoadImageAction, BuildModelAction, \
@@ -66,7 +65,7 @@ class CHMRunner:
 if __name__ == '__main__':
     runner = CHMRunner(base_actions)
 
-    eval_dataset_dir = '/data/cwip_eval'
+    eval_dataset_dir = '/data/cwip_v0_full'
     pt_dir = 'test_pts_cwip'
     os.makedirs(pt_dir, exist_ok=True)
     for d in tqdm(os.listdir(eval_dataset_dir)):
@@ -78,44 +77,44 @@ if __name__ == '__main__':
 
     pt_dir = 'test_pts_cwip'
     pt_files = np.array(natsorted(glob.glob(os.path.join(pt_dir, '*.pt'))))
-    diff_data = []
-    for name1, name2 in tqdm(list(progressive_for(os.listdir(eval_dataset_dir), n=2))):
-        pt_file_1 = os.path.join(pt_dir, f'{name1}.pt')
-        pt_file_2 = os.path.join(pt_dir, f'{name2}.pt')
-
-        mean_diff = runner.compare_feats(pt_file_1, pt_file_2)
-        diff_data.append({
-            'name1': name1,
-            'count1': len(os.listdir(os.path.join(eval_dataset_dir, name1))),
-            'name2': name2,
-            'count2': len(os.listdir(os.path.join(eval_dataset_dir, name2))),
-            'diff': mean_diff.detach().numpy().item(),
-        })
-
-    df_diff = pd.DataFrame(diff_data)
-    print(df_diff)
-    print(df_diff['diff'].describe())
-    print(df_diff[df_diff['diff'] < 0.30])
-    df_diff.to_csv('test_data_diff_cwip.csv', index=False)
-
-    # same_data = []
-    # for name in tqdm(os.listdir(eval_dataset_dir)):
-    #     pt_file_1 = os.path.join(pt_dir, f'{name}.pt')
-    #     mean_diff = runner.compare_feats(pt_file_1, pt_file_1)
-    #     same_data.append({
-    #         'artist_id_1': name,
-    #         'pt_file_1': pt_file_1,
-    #         'count': len(os.listdir(os.path.join(eval_dataset_dir, name))),
+    # diff_data = []
+    # for name1, name2 in tqdm(list(progressive_for(os.listdir(eval_dataset_dir), n=2))):
+    #     pt_file_1 = os.path.join(pt_dir, f'{name1}.pt')
+    #     pt_file_2 = os.path.join(pt_dir, f'{name2}.pt')
+    #
+    #     mean_diff = runner.compare_feats(pt_file_1, pt_file_2)
+    #     diff_data.append({
+    #         'name1': name1,
+    #         'count1': len(os.listdir(os.path.join(eval_dataset_dir, name1))),
+    #         'name2': name2,
+    #         'count2': len(os.listdir(os.path.join(eval_dataset_dir, name2))),
     #         'diff': mean_diff.detach().numpy().item(),
     #     })
     #
-    # df_same = pd.DataFrame(same_data)
-    # print(df_same)
-    # print(df_same.describe())
-    #
-    # filtered_df_name = df_same[(df_same['diff'] >= 0.25) | df_same['diff'].isna()]
-    # print(filtered_df_name)
-    # df_same.to_csv('test_data_same_cwip.csv', index=False)
+    # df_diff = pd.DataFrame(diff_data)
+    # print(df_diff)
+    # print(df_diff['diff'].describe())
+    # print(df_diff[df_diff['diff'] < 0.30])
+    # df_diff.to_csv('test_data_diff_cwip.csv', index=False)
+
+    same_data = []
+    for name in tqdm(os.listdir(eval_dataset_dir)):
+        pt_file_1 = os.path.join(pt_dir, f'{name}.pt')
+        mean_diff = runner.compare_feats(pt_file_1, pt_file_1)
+        same_data.append({
+            'artist_id_1': name,
+            'pt_file_1': pt_file_1,
+            'count': len(os.listdir(os.path.join(eval_dataset_dir, name))),
+            'diff': mean_diff.detach().numpy().item(),
+        })
+
+    df_same = pd.DataFrame(same_data)
+    print(df_same)
+    print(df_same.describe())
+
+    filtered_df_name = df_same[(df_same['diff'] >= 0.25) | df_same['diff'].isna()]
+    print(filtered_df_name)
+    df_same.to_csv('test_data_same_cwip.csv', index=False)
 
     # runner.extract_images_save_feat('test_cls/4', 'test_feat_x2.pt')
     # runner.extract_images_save_feat('test_cls/7', 'test_feat_x3.pt')
