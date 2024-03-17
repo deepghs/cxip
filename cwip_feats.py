762fa51b2ba777bfb9c2e8616ca71ed71ab90e4c
narugo1992 <narugo992@gmail.com>
2024-03-18 02:08:51 +0800

    dev(narugo): save that

--- cwip_feats.py
+++ cwip_feats.py
@@ -78,6 +78,24 @@ if __name__ == '__main__':
 
     pt_dir = 'test_pts_cwip'
     pt_files = np.array(natsorted(glob.glob(os.path.join(pt_dir, '*.pt'))))
+    same_data = []
+    for name in tqdm(os.listdir(eval_dataset_dir)):
+        pt_file_1 = os.path.join(pt_dir, f'{name}.pt')
+        mean_diff = runner.compare_feats(pt_file_1, pt_file_1)
+        same_data.append({
+            'name': name,
+            'count': len(os.listdir(os.path.join(eval_dataset_dir, name))),
+            'diff': mean_diff.detach().numpy().item(),
+        })
+
+    df_same = pd.DataFrame(same_data)
+    print(df_same)
+    print(df_same.describe())
+
+    filtered_df_name = df_same[(df_same['diff'] >= 0.25) | df_same['diff'].isna()]
+    print(filtered_df_name)
+    df_same.to_csv('test_data_same_cwip.csv', index=False)
+
     diff_data = []
     for name1, name2 in tqdm(list(progressive_for(os.listdir(eval_dataset_dir), n=2))):
         pt_file_1 = os.path.join(pt_dir, f'{name1}.pt')
@@ -97,24 +115,6 @@ if __name__ == '__main__':
     print(df_diff['diff'].describe())
     print(df_diff[df_diff['diff'] < 0.30])
     df_diff.to_csv('test_data_diff_cwip.csv', index=False)
-
-    same_data = []
-    for name in tqdm(os.listdir(eval_dataset_dir)):
-        pt_file_1 = os.path.join(pt_dir, f'{name}.pt')
-        mean_diff = runner.compare_feats(pt_file_1, pt_file_1)
-        same_data.append({
-            'name': name,
-            'count': len(os.listdir(os.path.join(eval_dataset_dir, name))),
-            'diff': mean_diff.detach().numpy().item(),
-        })
-
-    df_same = pd.DataFrame(same_data)
-    print(df_same)
-    print(df_same.describe())
-
-    filtered_df_name = df_same[(df_same['diff'] >= 0.25) | df_same['diff'].isna()]
-    print(filtered_df_name)
-    df_same.to_csv('test_data_same_cwip.csv', index=False)
 
     # runner.extract_images_save_feat('test_cls/4', 'test_feat_x2.pt')
     # runner.extract_images_save_feat('test_cls/7', 'test_feat_x3.pt')
